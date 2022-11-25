0f86cd0d2c927a66e9debc6c2d508f83ed20629a
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2022-11-24 16:46:01 -0800

    Initial Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Movie Picker",
+    imgUrl: "/assets/MoviePicker.PNG",
+    stack: ["HTML", "CSS", "JavaScript", "Flask", "Jinja", "Bootstrap"],
+    link: "https://moviepicker.kskyi.com",
+  },
+  {
+    title: "Portfolio",
+    imgUrl: "/assets/MoviePicker.PNG",
+    stack: ["React", "Vite", "Tailwind"],
+    link: "https://kskyi.com",
+  },
+  {
+    title: "Haven't created yet",
+    imgUrl: "/assets/MoviePicker.PNG",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://name_here.kskyi.com",
+  },
+];
---
+++ src/data/timeline.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    year: "Today",
+    title: "Freelance Dev",
+    duration: "half a year",
+    details: "My coding story, might get better in future, but that how it is.",
+  },
+  {
+    year: "2022",
+    title: "Fleet Transportation Manager",
+    duration: "5 and a half years",
+    details:
+      "For last over half of the decade I've been managing fleet of 15 drivers on average and few",
+  },
+  {
+    year: "2014",
+    title: "Bachelor's Degree in Computer Engineering",
+    duration: "4 years",
+    details:
+      "I pursued the knowledge that a computer Engineering degree had to offer. I learned about computer architecture, computer science theory, an array of programming and aspects of artificial intelligence.",
+  },
+];
