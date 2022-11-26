450f7e7c5f910aaa8ade13a2b8321e54bdccd6ee
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2022-11-25 17:44:13 -0800

    updated button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,8 +30,8 @@ function Contact() {
             className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
           />
           <button
-            type="button"
-            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-zinc-800 to-blue-700 drop-shadow-md hover:stroke-white"
+            type="submit"
+            className="text-center mx-auto inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-zinc-800 to-blue-700 drop-shadow-md hover:stroke-white"
           >
             Contact Me
           </button>
