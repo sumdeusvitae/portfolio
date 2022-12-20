3c0640c24ab948e18ea4c4dd37c80d48647594ad
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2022-12-20 11:44:11 -0500

    litttle fix in portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,14 +3,15 @@ import portfolio from "../data/portfolio";
 import PortfolioItem from "./PortfolioItem";
 import Title from "./Title";
 
-function Portfolio() {
+function Portfolio () {
   return (
     <>
       <div className="scale-125 flex justify-center mb-5">
         <Title>My Projects</Title>
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+          {/* Need to fix back to lg:grid-cols-2 */}
           {portfolio.map((project, id) => (
             <PortfolioItem
               key={id}
