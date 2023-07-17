e1ffad3839539e0b9668d5f676f2ff0161adef6b
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-07-17 15:56:54 -0500

    little tweek

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,7 @@ function Intro () {
         Hi, my name is Kyrylo
       </h1>
       <p className="text-2xl md:text-2xl mb-3 font-medium">
-        Web Developer & Software Engineer
+        ML Engineer & Software Developer
       </p>
       <p className="text-base max-w-xl mb-6 font-bold">
         I am a passionate engineer interested in artificial intelligence, data science, and web development. With a focus on real-world applications, I am eager to leverage my technical expertise and soft skills to contribute to innovative projects and make a meaningful impact. I have a strong background in engineering principles, which equips me with the necessary foundation to comprehend AI technologies and data analysis methodologies at a deep level. Additionally, my expertise in web development empowers me to design intuitive and user-friendly interfaces that result in seamless user experiences. Overall, I am driven by my enthusiasm for cutting-edge technologies and am eager to apply my knowledge and skills to drive progress and solve complex problems in the field.
