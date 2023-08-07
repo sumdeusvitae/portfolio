f0179a2336f9d246bcc6bdfc5f38463f660405fb
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-08-07 09:26:50 -0500

    s

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,7 +11,6 @@ function Portfolio () {
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-          {/* Need to fix back to lg:grid-cols-2 */}
           {portfolio.map((project, id) => (
             <PortfolioItem
               key={id}
