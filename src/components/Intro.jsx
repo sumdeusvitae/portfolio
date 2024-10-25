ffa1fa31f631e5f6080fc9e13043ec9c4092f86f
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-10-25 12:35:48 -0500

    struggle is real

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,12 +4,12 @@ function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 rounded-lg shadow-lg">
       <h2 className="text-4xl md:text-5xl dark:text-white mb-2 md:mb-4 font-bold tracking-wide">
-        Hello and welcome to my portfolio
+        Hello, welcome to my Portfolio
       </h2>
       <p className="text-2xl md:text-2xl mb-4 font-medium text-gray-700 dark:text-gray-300">
         A little bit about myself...
       </p>
-      <p className="text-base max-w-lg mb-6 font-normal text-gray-800 dark:text-gray-200 leading-relaxed">
+      <p className="text-base max-w-max mb-6 font-normal text-gray-800 dark:text-gray-200 leading-relaxed">
         I’m an engineer passionate about harnessing the power of artificial intelligence, data science, and web development to tackle real-world challenges. With a Bachelor’s Degree in Computer Engineering and hands-on experience as a Transportation Manager, I thrive at the intersection of technology and practical application.
 
         I’ve delved deep into machine learning, recently completing a specialization that sharpened my skills in building neural networks. My journey includes roles in QA testing, where I honed my attention to detail, and web development, where I enjoy creating user-friendly applications.
