cb6fc5f35c0d3cb3ba41a8659d2216c1241c2af2
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-10-27 11:11:33 -0500

    reverse

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,11 +6,9 @@ function Intro() {
       <h2 className="text-4xl md:text-5xl dark:text-white mb-2 md:mb-4 font-bold tracking-wide">
         Hello, welcome to my Portfolio
       </h2>
-      <div className="self-start text-left mb-4">
-        <p className="text-2xl md:text-2xl font-medium text-gray-700 dark:text-gray-300">
-          A little bit about myself...
-        </p>
-      </div>
+      <p className="text-2xl md:text-2xl mb-4 font-medium text-gray-700 dark:text-gray-300 text-left">
+        A little bit about myself...
+      </p>
       <p className="text-base max-w-max mb-6 font-normal text-gray-800 dark:text-gray-200 leading-relaxed">
         I’m an engineer passionate about harnessing the power of artificial intelligence, data science, and web development to tackle real-world challenges. With a Bachelor’s Degree in Computer Engineering and hands-on experience as a Transportation Manager, I thrive at the intersection of technology and practical application.
 
