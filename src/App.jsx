c1e6f5cb091348d965504b06986549fd8a60e735
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-10-31 13:01:55 -0500

    fixed scaling issue in mid-small res

--- src/App.jsx
+++ src/App.jsx
@@ -71,8 +71,8 @@ function App() {
       >
         {theme === "dark" ? sun : moon}
       </button>
-      <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter ">
-        <div className="max-w-5xl w-11/12 mx-auto">
+      <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter overflow-hidden">
+        <div className="max-w-full w-full mx-auto">
           <Intro />
           <Portfolio />
           <Timeline />
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Intro() {
   return (
-    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h2 className="text-4xl md:text-5xl dark:text-white mb-2 md:mb-4 font-bold tracking-wide">
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 px-4">
+      <h2 className="text-4xl md:text-5xl dark:text-white mb-2 md:mb-4 font-bold tracking-wide max-w-xl mx-auto">
         Hello, welcome to my Portfolio
       </h2>
-      <p className="text-2xl md:text-2xl mb-4 font-medium text-gray-700 dark:text-gray-300 text-left">
+      <p className="text-2xl md:text-2xl mb-4 font-medium text-gray-700 dark:text-gray-300 max-w-xl mx-auto">
         A little bit about myself...
       </p>
-      <p className="text-base max-w-max mb-6 font-normal text-gray-800 dark:text-gray-200 leading-relaxed">
+      <p className="text-l md:text-l mb-6 font-medium text-gray-800 dark:text-gray-200 leading-relaxed max-w-xl mx-auto">
         I’m an engineer passionate about harnessing the power of artificial intelligence, data science, and web development to tackle real-world challenges. With a Bachelor’s Degree in Computer Engineering and hands-on experience as a Transportation Manager, I thrive at the intersection of technology and practical application.
 
         I’ve delved deep into machine learning, recently completing a specialization that sharpened my skills in building neural networks. My journey includes roles in QA testing, where I honed my attention to detail, and web development, where I enjoy creating user-friendly applications.
