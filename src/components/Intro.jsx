dd294a6ed763b64f8170bd15c375fb4a49a82d51
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2025-01-22 14:29:46 -0600

    skills update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,31 +13,42 @@ function Intro() {
         <ul className="text-l md:text-l mb-6 font-medium text-gray-800 dark:text-gray-200 leading-relaxed max-w-xl mx-auto list-none">
           <li className="mb-2 flex items-start">
             <span className="mr-2 text-xl">•</span>
-            <span>GO, Python, JavaScript, C</span>
+            <span className="font-semibold">Programming:</span>
+            <ul className="pl-6 space-y-2">
+              <li>Python, Go, C</li>
+              <li>Algorithms, OOP, Functional programming</li>
+            </ul>
           </li>
-          <li className="mb-2 flex items-start">
+          <li className="flex items-start space-x-2">
             <span className="mr-2 text-xl">•</span>
-            <span>PostgreSQL, SQLite, MongoDB</span>
+            <span className="font-semibold">Tools:</span>
+            <ul className="pl-6 space-y-2">
+              <li>Git, Docker, Kubernetes</li>
+              <li>CI/CD, GitHub Actions</li>
+            </ul>
           </li>
-          <li className="mb-2 flex items-start">
+          <li className="flex items-start space-x-2">
             <span className="mr-2 text-xl">•</span>
-            <span>Docker, GCP, AWS</span>
+            <span className="font-semibold">Web Dev:</span>
+            <ul className="pl-6 space-y-2">
+              <li>SQL, REST APIs, Static Sites</li>
+              <li>Cloud (AWS, GCP)</li>
+            </ul>
           </li>
-          <li className="mb-2 flex items-start">
+          <li className="flex items-start space-x-2">
             <span className="mr-2 text-xl">•</span>
-            <span>Git, GitHub, GitHub Actions</span>
-          </li>
-          <li className="mb-2 flex items-start">
-            <span className="mr-2 text-xl">•</span>
-            <span>CNN, Deep Learning, Machine Learning</span>
+            <span className="font-semibold">Machine Learning:</span>
+            <ul className="pl-6 space-y-2">
+              <li>CNN, RNN, ANN, Transformers</li>
+              <li>TensorFlow, Deep Learning</li>
+            </ul>
           </li>
         </ul>
       </div>
     </div>
-
-
-
   );
 }
+
+
 
 export default Intro;
